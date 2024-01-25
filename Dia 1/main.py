660c0174caf65bd74b0e97da72fa08e052317174
Deivid <deividvelasquez122@gmail.com>
2024-01-25 09:03:11 -0500

    crear archivo

---
+++ Dia 1/main.py
@@ -0,0 +1,25 @@
+## ---------------------------------
+## ---- Ejercicio 1 ----
+## ---------------------------------
+
+## Impresión en consola 
+print('Hola mundo') 
+
+# --- Datos primitivos ---
+# 1 Sting
+texto = 'Campus'
+print(texto)
+print(type(texto))
+# 2 Int
+numeroEntero = 1
+print(numeroEntero) 
+# 3 Float 
+numeroDecimal = 4.5
+print(numeroDecimal) 
+# 4 Double
+numeroDecimalLargo = 3.1456555455
+print(numeroDecimalLargo)
+# 5 Boolean
+booleano = True
+print(booleano)
+## Desarrollado por DEIVID VELASQUEZ GUTIERREZ - 1096701633
